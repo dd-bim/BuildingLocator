476ce7c3fef9ee7cdaa579a9b61e70079528a4d8
Tim Kaiser <tim.kaiser@htw-dresden.de>
2018-11-22 08:55:34 +0100

    moved rotationStyle creation to customStyles.js

--- src/customStyles.js
+++ src/customStyles.js
@@ -1,5 +1,11 @@
-import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style.js';
+import {Circle as CircleStyle} from 'ol/style.js';
+import Style from 'ol/style/Style';
+import RegularShape from 'ol/style/RegularShape'
+import Fill from 'ol/style/Fill'
+import Stroke from 'ol/style/Stroke'
+import Text from 'ol/style/Text'
 import Point from 'ol/geom/Point';
+import LineString from 'ol/geom/LineString';
 
 
 export function getSelectStyle() {
@@ -53,4 +59,89 @@ export function getDeleteStyle() {
 
   return customStyle;
 
+}
+
+export function getRotateStyle() {
+  let white = [ 255, 255, 255, 0.8 ]
+  let blue = [ 0, 153, 255, 0.8 ]
+  let red = [ 209, 0, 26, 0.9 ]
+  let width = 2
+
+  let styles = {
+    anchor: [
+      new Style({
+        image: new RegularShape({
+          fill: new Fill({
+            color: blue
+          }),
+          stroke: new Stroke({
+            color: blue,
+            width: 1
+          }),
+          radius: 4,
+          points: 6
+        }),
+        zIndex: Infinity
+      })
+    ],
+    arrow: [
+      new Style({
+        stroke: new Stroke({
+          color: white,
+          width: width + 3,
+          lineDash: [ 10, 10 ]
+        }),
+        text: new Text({
+          font: '14px sans-serif',
+          offsetX: 25,
+          offsetY: -25,
+          fill: new Fill({
+            color: 'blue'
+          }),
+          stroke: new Stroke({
+            color: white,
+            width: width + 1
+          })
+        }),
+        zIndex: Infinity
+      }),
+      new Style({
+        stroke: new Stroke({
+          color: red,
+          width: width + 1,
+          lineDash: [ 10, 10 ]
+        }),
+        zIndex: Infinity
+      })
+    ]
+  }
+  return function (feature, resolution) {
+    let style
+    let angle = feature.get('angle') || 0
+
+    switch (true) {
+      case feature.get('rotate-anchor'):
+        style = styles[ 'anchor' ]
+        style[ 0 ].getImage().setRotation(-angle)
+
+        return style
+      case feature.get('rotate-arrow'):
+        style = styles[ 'arrow' ]
+
+        let coordinates = feature.getGeometry().getCoordinates()
+        // generate arrow polygon
+        let geom = new LineString([
+          coordinates,
+          [ coordinates[ 0 ], coordinates[ 1 ] + 100 * resolution ]
+        ])
+
+        // and rotate it according to current angle
+        geom.rotate(angle, coordinates)
+        style[ 0 ].setGeometry(geom)
+        style[ 1 ].setGeometry(geom)
+        style[ 0 ].getText().setText(Math.round(-angle * 180 / Math.PI) + '°')
+
+        return style
+    }
+  }
 }
